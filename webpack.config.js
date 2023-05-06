be4678eab1205b25ce296078200d4b224c7e4aad
Emily Doherty <git@emily.sh>
2023-05-06 15:43:36 -0700

    Use hashed bundle name for better caching and invalidation

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'window',
+    clean: true,
   },
 };
