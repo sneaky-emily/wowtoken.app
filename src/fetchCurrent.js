fa058cc64e061742a04cb7fa8283d78bcd7c0755
Emily Doherty <git@emily.sh>
2024-10-31 22:17:57 -0700

    Swap to v2 of current API

--- src/fetchCurrent.js
+++ src/fetchCurrent.js
@@ -1,4 +1,4 @@
 export default async function fetchCurrent() {
-    const resp = await fetch("https://data.wowtoken.app/token/current.json");
+    const resp = await fetch("https://data.wowtoken.app/v2/current/retail.json");
     return await resp.json();
 }
